8d5271fec596a115d2aa72ff468eb7d854a64513
adematti <arnaud.de-mattia@cea.fr>
2022-02-25 11:22:01 -0800

    refactor install_modules.sh

--- cosmodesimodules/install_modules.sh
+++ cosmodesimodules/install_modules.sh
@@ -1,11 +1,19 @@
-rootdir=$(realpath $COSMODESICONDA/../../../../)
-basedir=$COSMODESICONDA/../../startup/modulefiles/cosmodesimodules
-mkdir -p $basedir
-
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cp $script_dir/* $basedir
 
-ln -sf $basedir/activate_cosmodesi_jupyter.sh $rootdir/activate_cosmodesi_jupyter.sh
-ln -sf $basedir/activate_cosmodesi_jupyter.csh $rootdir/activate_cosmodesi_jupyter.csh
-ln -sf $basedir/cosmodesi_environment.sh $rootdir/cosmodesi_environment.sh
-ln -sf $basedir/cosmodesi_environment.csh $rootdir/cosmodesi_environment.csh
+if [ -z $CONF ] ; then CONF=nersc;   fi
+CONFDIR=$script_dir/../conf
+CONFIGUREENV=$CONFDIR/$CONF-env.sh
+source $CONFIGUREENV
+
+ROOTDIR=$(realpath $COSMODESICONDA/../../../../)
+MODULEDIR=$COSMODESICONDA/../../startup/modulefiles/cosmodesimodules
+mkdir -p $MODULEDIR
+
+cp $script_dir/* $MODULEDIR
+chgrp -R $GRP $MODULEDIR
+chmod -R u=rwX,g=rX,o-rwx $MODULEDIR
+
+scripts="activate_cosmodesi_jupyter.sh activate_cosmodesi_jupyter.csh cosmodesi_environment.sh cosmodesi_environment.csh"
+for script in $scripts ; do
+    ln -sf $MODULEDIR/$script $ROOTDIR/$script
+done
