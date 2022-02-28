af45a13e6b33f3b3ed809ad79bd7911c509d2ed2
adematti <arnaud.de-mattia@cea.fr>
2022-02-28 12:23:24 -0800

    add dill pip package for abacusutils

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -4,7 +4,7 @@ echo Installing pip packages at $(date)
 # see https://docs.nersc.gov/development/languages/python/parallel-python/
 MPICC="mpicc" pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
 MPICC="mpicc" pip install --no-cache-dir --no-binary=pmesh pmesh
-pip install --no-cache-dir --no-deps camb emcee schwimmbad getdist dynesty
+pip install --no-cache-dir --no-deps camb emcee schwimmbad getdist dynesty dill
 
 if [ $? != 0 ]; then
     echo "ERROR installing pip packages; exiting"
