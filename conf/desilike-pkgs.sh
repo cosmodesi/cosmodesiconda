0ab252736fc23b6c342c2358faf50579807a5d89
adematti <arnaud.de-mattia@cea.fr>
2023-08-03 02:37:19 -0700

    adding folps and pybird as modules

--- conf/desilike-pkgs.sh
+++ conf/desilike-pkgs.sh
@@ -3,5 +3,4 @@ export DESILIKE_CONFIG_DIR=$DESILIKE_INSTALL_DIR/.desilike
 mkdir -p $DESILIKE_CONFIG_DIR
 export ENVVARIABLES="$ENVVARIABLES DESILIKE_CONFIG_DIR $DESILIKE_CONFIG_DIR DESILIKE_INSTALL_DIR $DESILIKE_INSTALL_DIR"
 
-pip install --no-cache-dir --no-deps git+https://github.com/adematti/pybird@dev
 CC="cc" CFLAGS="" pip install --no-binary=:all: --no-cache-dir git+https://github.com/adematti/PolyChordLite@mpi4py
